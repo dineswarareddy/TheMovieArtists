f811c0bf86ac4471c77846665ee78b38dc84cc3a
dineswarareddy <dineswarareddy@gmail.com>
2024-12-30 19:44:30 +0530

    initial commit with project creation

---
+++ MovieArtists/MovieArtists/MovieArtistsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieArtistsApp.swift
+//  MovieArtists
+//
+//  Created by Dineswarareddy on 30/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieArtistsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MovieArtists/MovieArtistsTests/MovieArtistsTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieArtistsTests.swift
+//  MovieArtistsTests
+//
+//  Created by Dineswarareddy on 30/12/24.
+//
+
+import Testing
+@testable import MovieArtists
+
+struct MovieArtistsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
