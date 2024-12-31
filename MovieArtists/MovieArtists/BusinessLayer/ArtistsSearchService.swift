93b13ae49406a1907a89c2d1b2fdeab747a24a35
dineswarareddy <dineswarareddy@gmail.com>
2024-12-31 15:57:14 +0530

    Added service layer for artists search.

--- MovieArtists/MovieArtists/BusinessLayer/ArtistsSearchService.swift
+++ MovieArtists/MovieArtists/BusinessLayer/ArtistsSearchService.swift
@@ -7,6 +7,27 @@
 
 import Foundation
 
-class ArtistsSearchService {
-  
+protocol ArtistsSearchService {
+  func searchArtists(query: String,
+                     pageIndex: Int,
+                     completion: @escaping (Result<Data, Error>) -> Void)
+}
+
+class ArtistsSearchServiceImpl: ArtistsSearchService {
+  func searchArtists(query: String,
+                     pageIndex: Int,
+                     completion: @escaping (Result<Data, Error>) -> Void) {
+    let requestURL = URLConstructor(operationType: .searchArtists(query: query, pageIndex: pageIndex)).getRequestURL()
+    let request = NetworkRequest(endpointURL: requestURL,
+                                 httpMethod: OperationType.searchArtists(query: query,
+                                                                         pageIndex: pageIndex).httpMethod,
+                                 data: nil)
+    RestClient().send(request: request) { response, error in
+      if let responseData = response?.data {
+        completion(.success(responseData))
+        return
+      }
+      completion(.failure(SearchArtistsError.searchArtistsError))
+    }
+  }
 }
