806e3b075ff021c67d3fb3bf6f97e3b5f0a53c78
dineswarareddy <dineswarareddy@gmail.com>
2024-12-31 14:51:21 +0530

    Modified navigating to artist list view instead of content view.

--- MovieArtists/MovieArtists/MovieArtistsApp.swift
+++ MovieArtists/MovieArtists/MovieArtistsApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MovieArtistsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+          ArtistsListView(viewModel: ArtistListViewModel())
         }
     }
 }
