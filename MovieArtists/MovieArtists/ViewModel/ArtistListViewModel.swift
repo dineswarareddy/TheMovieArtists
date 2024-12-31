1f64a81a4bff078020e4574af84b5f2a3da11865
dineswarareddy <dineswarareddy@gmail.com>
2024-12-31 16:09:00 +0530

    Integrated usecase with artist list view model for search feature.

--- MovieArtists/MovieArtists/ViewModel/ArtistListViewModel.swift
+++ MovieArtists/MovieArtists/ViewModel/ArtistListViewModel.swift
@@ -13,12 +13,16 @@ class ArtistListViewModel: ObservableObject {
   @Published var isLoading: Bool = false
   @Published var firstResponseReceived: Bool = false
   @Published var searchingKeyword = ""
+  @Binding var searchText: String = ""
   
   var currentIndex = 1
   var usecase: ArtistListUsecase
+  var searchArtistsUsecase: ArtistSearchUsecase
   
-  init(usecase: ArtistListUsecase = ArtistListUsecaseImpl()) {
+  init(usecase: ArtistListUsecase = ArtistListUsecaseImpl(),
+       searchArtistsUsecase: ArtistSearchUsecase = ArtistSearchUsecaseImpl()) {
     self.usecase = usecase
+    self.searchArtistsUsecase = searchArtistsUsecase
   }
   
   func getNextPageContent() {
@@ -28,6 +32,22 @@ class ArtistListViewModel: ObservableObject {
   
   func fetchArtists() {
     usecase.fetchArtists(pageIndex: currentIndex) { [weak self] response in
+      DispatchQueue.main.async {
+        self?.firstResponseReceived = true
+        switch response {
+        case .success(let artistResponse):
+          self?.artists.append(contentsOf: artistResponse.results)
+          print(self?.artists)
+        case .failure(let error):
+          print("error received")
+        }
+      }
+    }
+  }
+  
+  func searchArtists() {
+    searchArtistsUsecase.searchArtists(query: $searchText,
+                                       pageIndex: currentIndex) { [weak self] response in
       DispatchQueue.main.async {
         self?.firstResponseReceived = true
         switch response {
