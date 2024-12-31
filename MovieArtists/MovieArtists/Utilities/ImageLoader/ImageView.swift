cecd35e3fcc6cf9902dcf4290d8d788663d7c351
dineswarareddy <dineswarareddy@gmail.com>
2024-12-31 14:42:41 +0530

    Added utility class which supports image view loading and caching.

---
+++ MovieArtists/MovieArtists/Utilities/ImageLoader/ImageView.swift
@@ -0,0 +1,21 @@
+
+import SwiftUI
+
+struct ImageView: View {
+    @ObservedObject private var imageViewModel: ImageViewModel
+    
+    init(urlString: String?) {
+        imageViewModel = ImageViewModel(urlString: urlString)
+    }
+    
+    var body: some View {
+        Image(uiImage: imageViewModel.image ?? UIImage())
+            .resizable()
+    }
+}
+
+struct ImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageView(urlString: "https://developer.apple.com/news/images/og/swiftui-og.png")
+    }
+}
